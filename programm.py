26703d97c074453b49ad647c43cfb40e3d7bb9cd
ctoic <i202612@nu.edu.pk>
2023-02-05 21:46:36 +0500

    Added python files

---
+++ programm.py
@@ -0,0 +1,12 @@
+pi = 3.14159 #constant
+
+radius = 2.2
+# area of circle
+area = pi*(radius**2)
+print('Area of circle with radius', radius, 'is', area)
+# Increment radius by 1
+radius = radius+1
+
+area_2 = pi*(radius**2)
+print('Area of circle with radius', radius, 'is', area_2)
+print(radius)
